687b90b0d6670fd3bf540cb80022708b6c796439
Lukas HORNE <0522561@schoolsnet.act.edu.au>
2026-09-14 15:10:04 +1000

    Update ultrasonic sensor with motor.ino

--- ultrasonic sensor with motor.ino
+++ ultrasonic sensor with motor.ino
@@ -10,36 +10,30 @@ const int In4 = 10;
 
 
 void setup() {
-  //ultrasonic input output
+  //setup ultrasonic sensor
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  // motor control pins outputs
+  //setup motor
   pinMode(In1, OUTPUT);
   pinMode(In2, OUTPUT);
   pinMode(In3, OUTPUT);
   pinMode(In4, OUTPUT);
 
-  // initialize serial communication:
   Serial.begin(9600);
 }
 
 void turnRight(){
-  // turn on motor A
   digitalWrite(In1, HIGH);
   digitalWrite(In2, LOW);
 }
 
 void turnLeft(){
-  // turn on motor A
   digitalWrite(In3, HIGH);
   digitalWrite(In4, LOW);
 }
 
 void loop(){
-  // Variables for duration of ping and the distance in cm
   long duration, cm;
-  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -57,7 +51,7 @@ void loop(){
   delay(10);
 
   if(cm < 6 && cm != 0){
-    //turn both motors on to move forward
+    //move forward
     turnLeft();
     turnRight();
   }
