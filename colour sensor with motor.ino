caf687265ed1c07fc2c93cb6ad556b311b80b3bb
Lukas HORNE <0522561@schoolsnet.act.edu.au>
2026-09-14 15:10:11 +1000

    Update colour sensor with motor.ino

--- colour sensor with motor.ino
+++ colour sensor with motor.ino
@@ -4,13 +4,14 @@ const int In2 = 12;
 const int In3 = 11;
 const int In4 = 10;
 
-//colour sensor variables
+//colour sensor pins
 #define S0 4
 #define S1 5
 #define S2 6
 #define S3 7
 #define sensorOut 8
 
+//calibration ranges
 int redMin = 23; 
 int redMax = 191;
 int greenMin = 20;
@@ -27,21 +28,22 @@ int greenValue;
 int blueValue;
 
 void setup() {
-// All motor control pins are outputs
+//motor setup
 pinMode(In1, OUTPUT);
 pinMode(In2, OUTPUT);
 pinMode(In3, OUTPUT);
 pinMode(In4, OUTPUT);
 
-//colour sensor
+//colour sensor setup
 pinMode(S0, OUTPUT);
 pinMode(S1, OUTPUT);
 pinMode(S2, OUTPUT);
 pinMode(S3, OUTPUT);
 pinMode(sensorOut, INPUT);
-// Set Frequency scaling to 20%
+
 digitalWrite(S0,HIGH);
 digitalWrite(S1,LOW);
+
 Serial.begin(9600);
 }
 
@@ -58,6 +60,7 @@ bluePW = getBluePW();
 blueValue = map(bluePW, blueMin,blueMax,255,0);
 delay(200);
 
+//identify colour and move
 if(blueValue < 239 && greenValue < 187 && redValue < 220){
  Serial.println("purple");
  //turn right
@@ -90,40 +93,30 @@ motorsOFF();
 }
 }
 
-
-// Function to read Red Pulse Widths
+// read red
 int getRedPW() {
-
-// Set sensor to read Red only
 digitalWrite(S2,LOW);
 digitalWrite(S3,LOW);
 int PW;
 PW = pulseIn(sensorOut, LOW);
-// Return the value
 return PW;
 }
 
-// Function to read Green Pulse Widths
+// read green
 int getGreenPW() {
-
-// Set sensor to read Green only
 digitalWrite(S2,HIGH);
 digitalWrite(S3,HIGH);
 int PW;
 PW = pulseIn(sensorOut, LOW);
-// Return the value
 return PW;
 }
 
-// Function to read Blue Pulse Widths
+// read blue
 int getBluePW() {
-
-// Set sensor to read Blue only
 digitalWrite(S2,LOW);
 digitalWrite(S3,HIGH);
 int PW;
 PW = pulseIn(sensorOut, LOW);
-// Return the value
 return PW;
 }
 
